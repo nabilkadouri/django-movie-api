f57322c3ca7ce593c2a9e1cb5d8b57e63b06ed27
Nabil Kadouri <kadourinabil7@gmail.com>
2026-05-26 15:59:14 +0200

    feat: add category list, create, update and delete endpoints

--- movies/urls.py
+++ movies/urls.py
@@ -5,5 +5,6 @@ from .views.category_views import CategoryView
 urlpatterns = [
     path("movies/", MovieView.as_view(), name="movie-list"),
     path("movies/<int:pk>/", MovieDetailView.as_view(), name="movie-detail"),
-    path("categories/", CategoryView.as_view()),
+    path("categories/", CategoryView.as_view(), name="category-list"),
+    path("categories/<int:pk>/", CategoryView.as_view(), name="category-detail")
 ]
--- movies/views/category_views.py
+++ movies/views/category_views.py
@@ -1,8 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from movies.serializers import CategorySerializer
+from movies.models import Category
 
 class CategoryView(APIView):
     
@@ -18,3 +20,41 @@ class CategoryView(APIView):
             return Response(result,status=status.HTTP_201_CREATED)
             
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request):
+        
+        categories = Category.objects.all()
+        
+        serializer = CategorySerializer(
+            categories,
+            many=True
+        )
+        
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+    def patch(self, request, pk=None):
+
+        category = get_object_or_404(Category, pk=pk)
+        
+        serializer = CategorySerializer(
+            category,
+            data=request.data,
+            partial=True
+        )
+        
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    def delete(self, request, pk=None):
+        
+        category = get_object_or_404(Category, pk=pk)
+        
+        category.delete()
+        
+        return Response(status=status.HTTP_204_NO_CONTENT)
